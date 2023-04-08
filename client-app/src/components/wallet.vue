02d9d62b5ec43845087873ed315496ee895c9fdb
Jin <121144363+JiceJin@users.noreply.github.com>
2023-04-08 09:05:58 +0800

    实现消息弹窗

--- client-app/src/components/wallet.vue
+++ client-app/src/components/wallet.vue
@@ -9,7 +9,10 @@ export default {
       return {
         provider: null,
         account: null,
-
+        //图片拉伸相关
+        fits: ['fill', 'contain', 'cover', 'none', 'scale-down'],
+        //图片url
+        url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
       }
     },
     computed:{
@@ -38,10 +41,20 @@ export default {
           return
         }
         //调用mint
-        await (await this.Score.mint(this.account,0)).wait();
+        // await (await this.Score.mint(this.account,1)).wait(); //======为测试，先不执行======
+        this.open2();
       },
+      //初始化合约
       initContract(){
         this.Score = new ethers.Contract(scoreAdd.address,scoreAbi,this.signer);
+      },
+      //消息弹窗
+      open2() {
+        this.$notify({
+          title: '提示',
+          message: '这是一条不会自动关闭的消息',
+          duration: 0
+        });
       }
     },
     async created(){
@@ -55,7 +68,16 @@ export default {
     <div v-if="account">{{  account }}</div>
     <el-button v-if="!account" @click="connect">连接钱包</el-button>
     <el-button v-if="account"  @click="disconnect">断开连接</el-button>
-    <el-button v-if="account && isSubmitted"  @click="mint">Mint</el-button>
+    <!-- && isSubmitted -->
+    <el-button v-if="account"  @click="mint">Mint</el-button>
+    <!-- 图片 -->
+    <div class="block" v-for="fit in fits" :key="fit">
+      <span class="demonstration">{{ fit }}</span>
+      <el-image
+        style="width: 100px; height: 100px"
+        :src="url"
+        :fit="fit"></el-image>
+    </div>
 </div>
 </template>
 
